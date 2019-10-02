7b61f6ce7cb148566952f52e0a168909c80b4926
Francis Calizo <francisacalizo@gmail.com>
2019-10-02 16:15:45 -0400

    mumbling

---
+++ mumbling.js
@@ -0,0 +1,20 @@
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt
+
+function accum(str) {
+  let result = '';
+
+  str.split('').forEach((letter, idx) => {
+    let currentString = '';
+
+    for(let i=1; i <= idx+1; i++){
+       currentString += letter.toLowerCase();
+    }
+
+    currentString = currentString.charAt(0).toUpperCase() + currentString.slice(1)
+    idx == 0 ? result += currentString : result += ('-' + currentString)
+  })
+
+  return result;
+}
